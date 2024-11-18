a56deee0271939815f9599e8b5408bd4bb6d2bbc
ajinusa <ajinusa92@gmail.com>
2024-11-18 17:34:34 +0700

    tambahkan file ke branch

--- import-csv.py
+++ import-csv.py
@@ -2,4 +2,4 @@ import csv
 with open('username.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
-        print(', '.join(row))
+        print(', '.join(row)) 
